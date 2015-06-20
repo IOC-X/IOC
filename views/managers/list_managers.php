755527416d29c34808a5f3f20fdf39c713f8826e
ROCKY-SAM <abeysekaramadushanka14@gmail.com>
2015-06-20 22:37:24 +0530

    managers ui

---
+++ views/managers/list_managers.php
@@ -0,0 +1,76 @@
+
+<div class="btn-group btn-group-justified">
+    <a href="" class="btn btn-default">
+            <div class="icon-preview"><i class="mdi-action-account-circle"></i>
+            </div>
+    Manager List</a>
+
+    <a href="add_managers" class="btn btn-default">
+            <div class="icon-preview"><i class="mdi-social-person-add"></i>
+            </div>
+    add new Manager</a>
+
+    <a href="add_managers" class="btn btn-default">
+            <div class="icon-preview"><i class="mdi-notification-event-note"></i>
+            </div>
+    Shift Management</a>
+
+    <a href="database_managers" class="btn btn-default">
+            <div class="icon-preview"><i class="mdi-file-cloud-circle"></i>
+            </div>
+    Database Management</a>
+
+</div>
+<h1> </h1>
+<h2> </h2>
+
+<!-- end of managers bar -->
+
+<table class="table table-striped table-hover ">
+        <col style="width:10%">
+        <col style="width:10%">
+        <col style="width:40%">
+        <col style="width:20%">
+        <col style="width:20%">
+    <thead>
+        <tr>
+            <th>Member Number</th>
+            <th></th>
+            <th>Manager Name</th>
+            <th >Edit Manager</th>
+            <th>Delete Manager</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="info">
+<!-- ------------------------------------------------------------------ -->
+            <td>
+
+            </td>
+<!-- ------------------------------------------------------------------ -->       
+            <td>
+   
+             <div class="list-group">
+                <div class="list-group-item">
+                    <div class="row-picture">
+                        <img class="circle" src="http://lorempixel.com/56/56/people/1" alt="icon">
+                    </div>
+            </div>
+
+            </td>
+<!-- ------------------------------------------------------------------ -->
+            <td>Full Name</td>
+<!-- ------------------------------------------------------------------ -->
+            <td>
+                <div class="icon-preview"><i class="mdi-editor-mode-edit"></i>  </div>
+            </td>
+<!-- ------------------------------------------------------------------ -->
+            <td>
+                <div class="icon-preview"> <i class="mdi-content-remove-circle"></i>  </div>
+            </td>
+<!-- ------------------------------------------------------------------ -->
+        </tr>
+
+    </tbody>
+</table>
+
