fb84abf0345366932c2fde844a6f2ce48ba1f737
Rajika <inbox.rajika@gmail.com>
2015-06-20 16:43:56 +0530

    Path for Jquery

--- config/paths.php
+++ config/paths.php
@@ -2,5 +2,5 @@
 	define('URL', 'http://localhost/IOC/');
 	define('CSS', 'http://localhost/IOC/views/');
 	define('CSS1', 'http://localhost/IOC/views/css/');
-
+	define('JQuery','http://localhost/IOC/bower_components/jquery/dist/jquery.min.js');
 ?>
