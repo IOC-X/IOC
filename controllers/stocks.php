c7c61aff2607f9757c2f328b7b8a7e137914907b
Rajika <inbox.rajika@gmail.com>
2015-06-27 21:06:58 +0530

    Stocks controller modification

--- controllers/stocks.php
+++ controllers/stocks.php
@@ -6,11 +6,17 @@
 		public function index(){
 			$this->view->render('stocks/index',false);
 		}
-		public function morning_stocks(){
+		public function stocks(){
 			//require('models/Stocks_model.php');
 			//$model = new Stocks_model();
 			//$data = $model->getMorningStocks();
-			$this->view->render('stocks/morning_stocks',false);
+			$this->view->render('stocks/stocks',false);
+		}
+		public function morning_reading(){
+			$this->view->render('stocks/morning_reading',false);
+		}
+		public function evening_reading(){
+			$this->view->render('stocks/evening_reading',false);
 		}
 	}
 ?>
