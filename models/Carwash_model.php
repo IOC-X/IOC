e575cefc3ad0c9caa71d3c0f0afb6c2a28d16fe1
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-06-28 20:01:42 +0530

    model_fix

--- models/Carwash_model.php
+++ models/Carwash_model.php
@@ -1,10 +1,32 @@
 <?php
+include_once 'C:\wamp\www\IOC\libs\Database.php';
 	class Carwash_model extends Model{
 		function __construct(){
 			parent::__construct();
 		}
-		public function getPackages(){
+		public static function getPackages(){
 			
-		}
+			$st = $this->db->prepare("SELECT * FROM packages");
+			$st->execute();
+			$data = $st->fetchAll();
+			
+
+                       if ($data->num_rows > 0) {
+                            
+                            while($row = $result->fetch_assoc()) 
+                            {
+                                echo $row["name"];
+                                echo $row["description"];
+                                echo $row["time"];
+                                echo $row["price"];
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+
+        db::close();
+  
+                }
 	}
+	
 ?>
