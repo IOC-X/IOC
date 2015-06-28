8a25a843f8820c1dd49564345a3de3787c9b9b61
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-06-28 20:04:42 +0530

    gui_fix

--- views/carwash/packages.php
+++ views/carwash/packages.php
@@ -1,5 +1,7 @@
 	<div>
-		Morning stocks
-		<input type="text" class="">
+		<span class="label label-silver"><?php echo $row[ 'id' ]; ?></span><br>
+        <span class="label label-silver"><?php echo $row[ 'name' ]; ?></span><br>
+        <span class="label label-silver"><?php echo $row[ 'description' ]; ?></span><br>
+        <span class="label label-silver"><?php echo $row[ 'price' ]; ?></span><br>
 	</div>
 
