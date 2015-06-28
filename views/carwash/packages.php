3c2aca08cd24d8e66f9bc964527978a7189045fb
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-06-28 23:47:58 +0530

    retrieving from db

--- views/carwash/packages.php
+++ views/carwash/packages.php
@@ -1,7 +1,29 @@
-	<div>
-		<span class="label label-silver"><?php echo $row[ 'id' ]; ?></span><br>
-        <span class="label label-silver"><?php echo $row[ 'name' ]; ?></span><br>
-        <span class="label label-silver"><?php echo $row[ 'description' ]; ?></span><br>
-        <span class="label label-silver"><?php echo $row[ 'price' ]; ?></span><br>
+<div class="raw">
+            <table class="table table-striped table-bordered">
+		
+                    <thead>
+						<tr>
+							<th>Name</th>
+							<th>Description</th>
+							<th>Price</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+                    <tbody>
+						<?php foreach ($packages as $package) : ?>						
+							<tr>
+								<td><?php echo htmlentities($package->name);  ?></td>
+								<td><?php echo htmlentities($package->description); ?></td>
+								<td><?php echo htmlentities($package->price); ?></td>
+								<td>
+									<a class="btn btn-info" href="index.php?op=show&id=<?php echo $package->id; ?>">View</a>
+									<a class="btn btn-success" href="index.php?op=edit&id=<?php echo $package->id; ?>">Update</a>
+									<a class="btn btn-danger" href="index.php?op=delete&id=<?php echo $package->id; ?>">Delete Package</a>
+								</td>
+
+							</tr>
+						<?php endforeach; ?>
+					</tbody>
+            </table>
 	</div>
 
