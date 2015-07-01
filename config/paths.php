50bbe1d34d117190c9ed1055f17e28ca9934eaf9
Rajika <inbox.rajika@gmail.com>
2015-07-01 20:34:13 +0530

    Path for Chart.Js

--- config/paths.php
+++ config/paths.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 	define('URL', 'http://localhost/IOC/');
 	define('CSS', 'http://localhost/IOC/views/');
 	define('CSS1', 'http://localhost/IOC/views/css/');
 	define('JQuery','http://localhost/IOC/bower_components/jquery/dist/jquery.min.js');
+	define('ChartJS', 'http://localhost/IOC/bower_components/Chart.js/Chart.js');
 ?>
