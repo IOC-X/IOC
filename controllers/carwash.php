3d0cbb6903ebfab2998f6316052f2f49f0109de6
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-07-01 15:41:33 +0530

    edit,update

--- controllers/carwash.php
+++ controllers/carwash.php
@@ -10,9 +10,9 @@ include_once '/models/Carwash_model.php';
 		}
 		public function packages(){
                         $model = new Carwash_model();
-                        $packages= $model->getPackages();
+                        $packages= $model->selectAllpackages();
                     
-			$this->view->render('carwash/packages',false);
+			//$this->view->render('carwash/packages',false);
                         include '/views/carwash/packages.php';
 		}
                 public function transactions(){
@@ -33,5 +33,30 @@ include_once '/models/Carwash_model.php';
 			//$data = $model->getMorningStocks();
 			$this->view->render('carwash/report',false);
 		}
-	}
+                
+                public function edit($id=false){
+              //  $this->view->render('carwash/edit',false);
+                    $model = new Carwash_model();
+                    $name   = '';
+		$description  = '';
+		$price = '';
+		//$id     = $_GET['id'];
+                   $package=$model->selectById($id);
+                    $title = 'Edit package';
+                    echo $id;
+		
+                if (isset($_POST['form-submitted'])) 
+		{
+			$name   = isset($_POST['name'])   ? trim($_POST['name']) 	   : null;
+			$email  = isset($_POST['description'])  ? trim($_POST['description']) 	   : null;
+			$mobile = isset($_POST['price']) ? trim($_POST['price'])     : null;
+			{
+                    $packages= $model->edit($name, $description, $price,$id);
+                 
+                        }
+                }
+                include '/views/carwash/edit.php';
+                }
+        }           
+	
 ?>
