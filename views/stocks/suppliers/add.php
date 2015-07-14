082a5aa850107845cd944b7d771c5aeba6b03b68
Rajika <inbox.rajika@gmail.com>
2015-07-14 23:47:18 +0530

    Added UI for Suppliers insertion

--- views/stocks/suppliers/add.php
+++ views/stocks/suppliers/add.php
@@ -1,7 +1,33 @@
 <form class="form-horizontal" id="add_supplier_form" action="stocks/addSupplier" method="post">
     <fieldset>
-    <legend>Add new supplier</legend>
-        	
+    <legend>Add new suppliers</legend>
+        <div class="form-group">
+        <label for="sup-name" class="col-lg-2 control-label">Supplier name</label>
+        <div class="col-lg-7">
+            <input type="text" class="form-control" id="sup-name" placeholder="product name" name="sup-name">
+        </div>
+      </div>
+        <div class="form-group">
+        <label for="products" class="col-lg-2 control-label">Products</label>
+        <div class="col-lg-4">
+                <select id="products" placeholder="supplier" class="form-control" name="products">
+                  <option></option>
+                  <option value="fuel">Fuel</option>
+                  <option value="lubricants">Lubricants</option>
+                </select>
+        </div>
+      </div>
+        <div class="form-group">
+        <label for="tel-number" class="col-lg-2 control-label">Quantity</label>
+        <div class="col-lg-7">
+            <input type="text" class="form-control" id="tel-number" placeholder="contact number" name="prd-tel-number">
+        </div>
+      </div>
+        <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>  
     </fieldset>
 </form>
 <script>
