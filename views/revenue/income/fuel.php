ab4d5216f11fb6653575a1e2415b97ad6e025ba2
Sarasi Sumathipala <sarasi.sumathipala@gmail.com>
2015-07-29 12:20:51 +0530

    Text fields added
    Minor changes

--- views/revenue/income/fuel.php
+++ views/revenue/income/fuel.php
@@ -6,9 +6,72 @@
             <input type="text" class="form-control" id="prd-name" placeholder="amount" name="">
         </div>
     	</div>
-        <div class="col-lg-1">
+
+        
+
+        <div class="form-group">
+            <label for="fuel" class="col-lg-1 control-label">Fuel type</label>
+                <div class="col-lg-2">
+                <select id="fuel" placeholder="type of fuel" class="form-control" name="fuel">
+                  <option></option>
+                  <option value="petrol">Petrol</option>
+                  <option value="spetrol">Super Petrol</option>
+                  <option value="diesel">Diesel</option>
+                  <option value="sdiesel">Super Diesel</option>
+                </select>
+                </div>
+                
+                </div>
+
+                
+
+
+
+        <div class="form-group">
+        <label for="select-date" class="col-lg-1 control-label">Select time(date)</label>
+        <div class="col-lg-3">
+            <input type="text" class="form-control" id="select-date" placeholder="" name="">
+        </div>
+        </div> 
+
+
+        <div class="col-lg-10">
             
         </div>
+
+
+         
+                
+            
+
+
+        <!--div class="col-lg-1">
+            
+        </div-->
+
+            <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+                <button type="submit" class="btn btn-primary" >Submit</button>
+            </div>
+            </div> 
+
+        
+            
+         <div class="col-lg-10">
+            
+        </div>
+
+         <div class="col-lg-10">
+            
+        </div>
+
+         <div class="col-lg-10">
+            
+        </div>
+
+
+
+
         <div class="col-lg-7">
             <table class="table table-striped table-hover ">
                 <thead>
