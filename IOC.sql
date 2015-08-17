0979b1c2e1beddfc7bd04afc3b74a31b9b35417e
Rajika <inbox.rajika@gmail.com>
2015-08-17 22:47:32 +0530

    DB update

--- IOC.sql
+++ IOC.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 17, 2015 at 08:29 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Aug 17, 2015 at 07:16 PM
+-- Server version: 5.5.43-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ioc`
+-- Database: `IOC`
 --
 
 -- --------------------------------------------------------
@@ -50,7 +50,7 @@ INSERT INTO `car_transactions` (`id`, `cname`, `contact`, `email`, `package`, `v
 (9, 'zxczxc', '0711363825', 'isuru.dilhan@yahoo.com', 'Ultimate Value Car Wash', 'cxvxcv', 4500, '2015-08-01', 'Returned'),
 (10, 'isu', '0789164423', 'isuru.dilhan@yahoo.com', 'Ultimate Value Car Wash', 'you', 4500, '2015-08-16', 'Returned'),
 (11, 'fg', '0789164423', 'isuru.dilhan@yahoo.com', 'Ultimate Value Car Wash', 'fgg', 4500, '2015-08-16', 'Returned'),
-(12, 'fdsfds', '0711363825', 'isuru.dilhan@yahoo.com', 'WINDSCREEN & GLASS CLEANING- SILVER', 'uiyu', 5000, '2015-08-17', 'Not Returned');
+(12, 'fdsfds', '0711363825', 'isuru.dilhan@yahoo.com', 'Ultimate Value Car Wash', 'uiyu', 5000, '2015-08-17', 'Not Returned');
 
 -- --------------------------------------------------------
 
@@ -60,33 +60,45 @@ INSERT INTO `car_transactions` (`id`, `cname`, `contact`, `email`, `package`, `v
 
 CREATE TABLE IF NOT EXISTS `eveningstocks` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `fueltype` varchar(2) NOT NULL,
-  `reading` float NOT NULL,
-  `amount` float NOT NULL,
+  `FuelType` varchar(2) NOT NULL,
+  `Reading` float NOT NULL,
+  `Quantity` float NOT NULL,
+  `Date` date NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `eveningstocks`
+--
+
+INSERT INTO `eveningstocks` (`Id`, `FuelType`, `Reading`, `Quantity`, `Date`) VALUES
+(1, 'Pe', 200, 400, '2015-08-17'),
+(2, 'SP', 700, 1400, '2015-08-17'),
+(3, 'Di', 0, 0, '2015-08-17'),
+(4, 'SD', 0, 0, '2015-08-17');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lubricants`
+-- Table structure for table `Lubricants`
 --
 
-CREATE TABLE IF NOT EXISTS `lubricants` (
+CREATE TABLE IF NOT EXISTS `Lubricants` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(30) NOT NULL,
   `Price` float NOT NULL,
   `Quantity` int(11) NOT NULL,
   `Supplier` varchar(30) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Dumping data for table `lubricants`
+-- Dumping data for table `Lubricants`
 --
 
-INSERT INTO `lubricants` (`Id`, `Name`, `Price`, `Quantity`, `Supplier`) VALUES
-(2, 'T2', 780, 4, 'T2 dude');
+INSERT INTO `Lubricants` (`Id`, `Name`, `Price`, `Quantity`, `Supplier`) VALUES
+(2, 'T2', 780, 4, 'T2 dude'),
+(4, 'K3', 1, 5, 'T2 dude');
 
 -- --------------------------------------------------------
 
@@ -167,10 +179,10 @@ INSERT INTO `nonreglu_transactions` (`id`, `cname`, `contact`, `email`, `package
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `Orders`
 --
 
-CREATE TABLE IF NOT EXISTS `orders` (
+CREATE TABLE IF NOT EXISTS `Orders` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `FuelType` varchar(10) NOT NULL,
   `Reading` float NOT NULL,
@@ -178,55 +190,25 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `Orderamnt` int(10) NOT NULL,
   `Date` date NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=119 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=159 ;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `Orders`
 --
 
-INSERT INTO `orders` (`Id`, `FuelType`, `Reading`, `Quantity`, `Orderamnt`, `Date`) VALUES
-(77, 'Diesel', 200, 400, 6600, '2015-08-02'),
-(78, 'SDiesel', 1200, 2400, 19800, '2015-08-02'),
-(79, 'Petrol', 0, 0, 6600, '2015-08-02'),
-(80, 'SPetrol', 0, 0, 6600, '2015-08-02'),
-(81, 'Diesel', 0, 0, 6600, '2015-08-02'),
-(82, 'SDiesel', 0, 0, 6600, '2015-08-02'),
-(83, 'Petrol', 700, 1400, 19800, '2015-08-02'),
-(84, 'SPetrol', 0, 0, 6600, '2015-08-02'),
-(85, 'Diesel', 0, 0, 6600, '2015-08-02'),
-(86, 'SDiesel', 0, 0, 6600, '2015-08-02'),
-(87, 'Petrol', 700, 1400, 19800, '2015-08-02'),
-(88, 'SPetrol', 200, 400, 19800, '2015-08-02'),
-(89, 'Diesel', 700, 1400, 6600, '2015-08-02'),
-(90, 'SDiesel', 1200, 2400, 19800, '2015-08-02'),
-(91, 'Petrol', 200, 400, 19800, '2015-08-02'),
-(92, 'SPetrol', 700, 1400, 19800, '2015-08-02'),
-(93, 'Diesel', 700, 1400, 6600, '2015-08-02'),
-(94, 'SDiesel', 200, 400, 19800, '2015-08-02'),
-(95, 'Petrol', 200, 400, 19800, '2015-08-02'),
-(96, 'SPetrol', 700, 1400, 19800, '2015-08-02'),
-(97, 'Diesel', 0, 0, 6600, '2015-08-02'),
-(98, 'SDiesel', 0, 0, 6600, '2015-08-02'),
-(99, 'Petrol', 0, 0, 6600, '2015-08-02'),
-(100, 'SPetrol', 0, 0, 6600, '2015-08-02'),
-(101, 'Diesel', 0, 0, 6600, '2015-08-02'),
-(102, 'SDiesel', 0, 0, 6600, '2015-08-02'),
-(103, 'Petrol', 0, 0, 6600, '2015-08-02'),
-(104, 'SPetrol', 0, 0, 6600, '2015-08-02'),
-(105, 'Diesel', 0, 0, 6600, '2015-08-02'),
-(106, 'SDiesel', 0, 0, 6600, '2015-08-02'),
-(107, 'Petrol', 200, 400, 19800, '2015-08-02'),
-(108, 'Petrol', 200, 400, 19800, '2015-08-02'),
-(109, 'SPetrol', 700, 1400, 19800, '2015-08-02'),
-(110, 'SPetrol', 700, 1400, 19800, '2015-08-02'),
-(111, 'Diesel', 700, 1400, 6600, '2015-08-02'),
-(112, 'Diesel', 700, 1400, 6600, '2015-08-02'),
-(113, 'SDiesel', 1200, 2400, 19800, '2015-08-02'),
-(114, 'SDiesel', 1200, 2400, 19800, '2015-08-02'),
-(115, 'Petrol', 200, 400, 19800, '2015-08-02'),
-(116, 'SPetrol', 700, 1400, 19800, '2015-08-02'),
-(117, 'Diesel', 700, 1400, 6600, '2015-08-02'),
-(118, 'SDiesel', 1200, 2400, 19800, '2015-08-02');
+INSERT INTO `Orders` (`Id`, `FuelType`, `Reading`, `Quantity`, `Orderamnt`, `Date`) VALUES
+(143, 'Petrol', 200, 44, 19800, '2015-08-17'),
+(144, 'SPetrol', 700, 1400, 19800, '2015-08-17'),
+(145, 'Diesel', 200, 400, 6600, '2015-08-17'),
+(146, 'SDiesel', 700, 1400, 19800, '2015-08-17'),
+(151, 'Petrol', 200, 400, 19800, '2015-08-17'),
+(152, 'SPetrol', 700, 1400, 19800, '2015-08-17'),
+(153, 'Diesel', 700, 1400, 6600, '2015-08-17'),
+(154, 'SDiesel', 1200, 2400, 19800, '2015-08-17'),
+(155, 'Petrol', 200, 400, 19800, '2015-08-17'),
+(156, 'SPetrol', 700, 1400, 19800, '2015-08-17'),
+(157, 'Diesel', 700, 1400, 6600, '2015-08-17'),
+(158, 'SDiesel', 200, 400, 19800, '2015-08-17');
 
 -- --------------------------------------------------------
 
@@ -262,17 +244,26 @@ CREATE TABLE IF NOT EXISTS `pumpreadings` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Date` date NOT NULL,
   `Reading` float NOT NULL,
-  `1` float NOT NULL,
-  `2` float NOT NULL,
-  `3` float NOT NULL,
-  `4` float NOT NULL,
-  `5` float NOT NULL,
-  `6` float NOT NULL,
-  `7` float NOT NULL,
-  `8` float NOT NULL,
-  `9` float NOT NULL,
+  `PumpNo` int(11) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `pumpreadings`
+--
+
+INSERT INTO `pumpreadings` (`Id`, `Date`, `Reading`, `PumpNo`) VALUES
+(1, '2015-08-16', 3, 0),
+(2, '2015-08-16', 34, 0),
+(3, '2015-08-16', 45, 0),
+(4, '2015-08-16', 56, 0),
+(7, '2015-08-16', 56, 3),
+(8, '2015-12-16', 34, 4),
+(9, '2015-08-16', 22, 7),
+(10, '2015-08-16', 33, 9),
+(12, '2015-08-16', 78, 2),
+(20, '2015-08-16', 88, 4),
+(27, '2012-02-01', 8, 1);
 
 -- --------------------------------------------------------
 
@@ -283,6 +274,7 @@ CREATE TABLE IF NOT EXISTS `pumpreadings` (
 CREATE TABLE IF NOT EXISTS `pumpstatus` (
   `PumpNo` int(11) NOT NULL,
   `Status` varchar(10) NOT NULL,
+  `Fuel` varchar(20) NOT NULL,
   PRIMARY KEY (`PumpNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -290,135 +282,40 @@ CREATE TABLE IF NOT EXISTS `pumpstatus` (
 -- Dumping data for table `pumpstatus`
 --
 
-INSERT INTO `pumpstatus` (`PumpNo`, `Status`) VALUES
-(1, 'Online'),
-(2, 'Online'),
-(3, 'Offline'),
-(4, ''),
-(5, ''),
-(6, ''),
-(7, ''),
-(8, ''),
-(9, '');
+INSERT INTO `pumpstatus` (`PumpNo`, `Status`, `Fuel`) VALUES
+(1, 'Online', 'SuperPetrol'),
+(2, 'Online', ''),
+(3, 'Offline', 'Petrol'),
+(4, 'Offline', 'SuperPetrol'),
+(5, 'Offline', 'Diesel'),
+(6, 'Offline', 'SuperDiesel'),
+(7, 'Online', 'Diesel'),
+(8, 'Offline', 'SuperDiesel'),
+(9, 'Offline', 'Petrol'),
+(10, 'Offline', 'SuperPetrol');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `regular_customers`
+-- Table structure for table `Suppliers`
 --
 
-CREATE TABLE IF NOT EXISTS `regular_customers` (
-  `cust_id` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `nic` varchar(10) NOT NULL,
-  `address` char(100) NOT NULL,
-  `contact` varchar(10) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `date` varchar(15) NOT NULL,
-  PRIMARY KEY (`cust_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `regular_customers`
---
-
-INSERT INTO `regular_customers` (`cust_id`, `name`, `nic`, `address`, `contact`, `email`, `date`) VALUES
-('rc-1018', 'Isuru', '123456899v', 'Gampaha', '0711363825', 'isuru.dilhan@yahoo.com', '2015-07-31'),
-('rc-1090', 'Dilhan', '123456789v', 'dsfdsf', '0711363825', 'isuru.dilhan@yahoo.com', '2015-07-31'),
-('rc-1122', '546456', 'sdfdsf', 'sdfsdf', '0711363825', 'isuru.dilhan@yahoo.com', '2015-07-31'),
-('rc-549', 'fgdfgdfg', 'fgdg', 'dfgdg', '0711363825', 'isuru.dilhan@yahoo.com', '2015-07-31'),
-('rc-892', 'fgdfg', '950028343v', 'sdfds', '4444444444', 'isuru.dils@yahoo.comm', '2015-07-31');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `regular_lutransactions`
---
-
-CREATE TABLE IF NOT EXISTS `regular_lutransactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cust_id` varchar(10) NOT NULL,
-  `package` varchar(50) NOT NULL,
-  `vehicleNo` varchar(15) NOT NULL,
-  `amount` float NOT NULL,
-  `date` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `regular_lutransactions`
---
-
-INSERT INTO `regular_lutransactions` (`id`, `cust_id`, `package`, `vehicleNo`, `amount`, `date`) VALUES
-(2, 'lc-436', 'UNDERCARRIAGE DEGREASING - GOLD', 'hyyyy', 6400, '2015-08-16'),
-(4, 'lc-436', 'UNDERCARRIAGE DEGREASING - GOLD', 'fd', 6400, '2015-08-17');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `regular_transactions`
---
-
-CREATE TABLE IF NOT EXISTS `regular_transactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cust_id` varchar(10) NOT NULL,
-  `package` varchar(50) NOT NULL,
-  `vehicleNo` varchar(15) NOT NULL,
-  `amount` float NOT NULL,
-  `date` varchar(15) NOT NULL,
-  `status` varchar(50) NOT NULL DEFAULT 'Not Returned',
-  PRIMARY KEY (`id`,`cust_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
-
---
--- Dumping data for table `regular_transactions`
---
-
-INSERT INTO `regular_transactions` (`id`, `cust_id`, `package`, `vehicleNo`, `amount`, `date`, `status`) VALUES
-(19, 'rc-892', 'Ultimate Value Car Wash', 'hyh', 3600, '2015-07-30', 'Returned'),
-(21, 'rc-1018', 'Ultimate Value Car Wash', 'dfgdfg', 3600, '2015-08-01', 'Returned'),
-(22, 'rc-1018', 'Ultimate Value Car Wash', 'sadasd', 3600, '2015-08-01', 'Returned'),
-(23, 'rc-1018', 'Ultimate Value Car Wash', 'ddfdf', 3600, '2015-08-01', 'Returned'),
-(24, 'rc-1018', 'Ultimate Value Car Wash', 'dds', 3600, '2015-08-01', 'Returned'),
-(26, 'rc-1018', 'Ultimate Value Car Wash', 'cxvcxv', 3600, '2015-08-01', 'Returned'),
-(27, 'rc-1018', 'Ultimate Value Car Wash', 'cvcxv', 3600, '2015-08-01', 'Returned'),
-(28, 'rc-1018', 'Ultimate Value Car Wash', 'dgdg', 3600, '2015-08-01', 'Returned'),
-(29, 'rc-1090', 'Ultimate Value Car Wash', 'zxczc', 3600, '2015-08-01', 'Returned'),
-(30, 'rc-1018', 'Ultimate Value Car Wash', 'sdfdsf', 3600, '2015-08-01', 'Returned'),
-(31, 'rc-1018', 'Ultimate Value Car Wash', 'vcvd', 3600, '2015-08-01', 'Returned'),
-(32, 'rc-1090', 'Hand Wash', 'jhfkjhf', 8000, '2015-08-01', 'Returned'),
-(33, 'rc-1090', 'Ultimate Value Car Wash', 'vhjghj', 3600, '2015-08-01', 'Returned'),
-(34, 'rc-1090', 'Ultimate Value Car Wash', 'cxvxcv', 3600, '2015-08-01', 'Returned'),
-(36, 'rc-1018', 'Ultimate Value Car Wash', 'hyu', 3600, '2015-08-02', 'Returned'),
-(37, 'rc-1090', 'Ultimate Value Car Wash', 'cat-897', 3600, '2015-08-02', 'Returned'),
-(38, 'rc-1018', 'Ultimate Value Car Wash', 'CAT-4949', 3600, '2015-08-02', 'Returned'),
-(39, 'rc-1090', 'Hand Wash', 'BAT-9090', 8000, '2015-08-02', 'Returned'),
-(40, 'rc-1090', 'Hand Wash', 'you', 8000, '2015-08-05', 'Returned'),
-(41, 'rc-1018', 'Platinum Value Car Wash', 'sdasd', 1600, '2015-08-05', 'Returned'),
-(42, 'rc-1018', 'Ultimate Value Car Wash', 'xcvxv', 3600, '2015-08-05', 'Returned'),
-(43, 'rc-1090', 'Ultimate Value Car Wash', 'ghdfgdd', 3600, '2015-08-16', 'Returned');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `suppliers`
---
-
-CREATE TABLE IF NOT EXISTS `suppliers` (
+CREATE TABLE IF NOT EXISTS `Suppliers` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `product` varchar(10) NOT NULL,
   `contact` int(11) NOT NULL,
   `email` varchar(20) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
 
 --
--- Dumping data for table `suppliers`
+-- Dumping data for table `Suppliers`
 --
 
-INSERT INTO `suppliers` (`Id`, `name`, `product`, `contact`, `email`) VALUES
-(41, 'T2 dude', 'lubricant', 717479370, 't2dude@gmail.com');
+INSERT INTO `Suppliers` (`Id`, `name`, `product`, `contact`, `email`) VALUES
+(41, 'T2 dude', 'lubricant', 717479370, 't2dude@gmail.com'),
+(42, 'T3 dude', 'lubricant', 717479370, 't3dude@gmail.com');
 
 -- --------------------------------------------------------
 
