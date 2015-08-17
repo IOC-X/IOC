7e940aec8264c382e9c2541235c6a8be64f040d4
Rajika <inbox.rajika@gmail.com>
2015-08-17 21:53:16 +0530

    Inline CSS

--- views/stocks/index.php
+++ views/stocks/index.php
@@ -5,15 +5,15 @@
 		    </div>
 		    <div class="panel-body">
 		        <a href="stocks/stocks" class="btn btn-flat btn-primary" id="stocks">
-		        <i class="mdi-action-trending-up"></i>Stocks</a>
+		        <i class="mdi-action-trending-up"  style="margin-right:10px"></i>Stocks</a>
 		        <a href="stocks/pump_readings" class="btn btn-flat btn-primary" id="pump_readings">
-		        <i class="mdi-image-tune"></i>Pump readings</a>
+		        <i class="mdi-image-tune" style="margin-right:10px"></i>Pump readings</a>
 		        <a href="stocks/lubricant" class="btn btn-flat btn-primary" id="lubricant">
-		        <i class="mdi-notification-drive-eta"></i>Lubricant Store</a>
+		        <i class="mdi-notification-drive-eta" style="margin-right:10px"></i>Lubricant Store</a>
 		        <a href="stocks/suppliers" class="btn btn-flat btn-primary" id="suppliers">
-		        <i class="mdi-maps-directions-bike"></i>Suppliers</a>
+		        <i class="mdi-maps-directions-bike" style="margin-right:10px"></i>Suppliers</a>
 		        <a href="stocks/history" class="btn btn-flat btn-primary" id="history">
-		        <i class="mdi-maps-layers"></i>Report</a>
+		        <i class="mdi-maps-layers" style="margin-right:10px"></i>Report</a>
 		    </div>
 		    <script type="text/javascript">
 		    function hashCheck(url){
