fad19d59564cd46fe10a82adfdf743684ada929d
Rajika <inbox.rajika@gmail.com>
2015-08-22 09:16:56 +0530

    New global

--- config/paths.php
+++ config/paths.php
@@ -4,6 +4,7 @@
 	define('IMG', 'http://localhost/IOC/views/');
 	define('FLOATING', 'http://localhost/IOC/node_modules/mfb/src/');
 	define('CSS1', 'http://localhost/IOC/views/css/');
+	define('BRANCHIMG','http://localhost/IOC/views/transport/branches/img/branchimg.jpg');
 	define('JQuery','http://localhost/IOC/bower_components/jquery/dist/jquery.min.js');
 	define('BOWER','http://localhost/IOC/bower_components/');
 	define('ChartJS', 'http://localhost/IOC/bower_components/Chart.js/Chart.js');
