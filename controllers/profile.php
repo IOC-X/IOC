94e1e7cab912332ad3b5e4265ef047e34cc9b814
Rajika <inbox.rajika@gmail.com>
2015-08-25 19:49:45 +0530

    Profile card

--- controllers/profile.php
+++ controllers/profile.php
@@ -6,5 +6,14 @@
 		public function index(){
 			$this->view->render('profile/index',false);
 		}
+		public function loadProfileCode(){
+			Session::init();
+			$employeeCode = $_SESSION['loggedIn'];
+			require 'models/Login_model.php';
+			$model = new Login_model();
+			//echo $model->loadProfileDetails($employeeCode);
+			echo json_encode($model->loadProfileDetails($employeeCode));
+//			echo json_encode($employeeCode);
+		}
 	}
 ?>
