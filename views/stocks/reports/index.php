50b9417f9328d22e8e86d1a8938c0e97baacaef3
Rajika <inbox.rajika@gmail.com>
2015-09-22 21:23:40 +0530

    Changed icons

--- views/stocks/reports/index.php
+++ views/stocks/reports/index.php
@@ -1,7 +1,7 @@
 <div class="btn-group btn-group-justified" id="pump-tabs">
     <a href="javascript:void(0)" class="btn btn-primary" id="stockreport"><i class="mdi-action-turned-in"></i>Stocks</a>
-    <a href="javascript:void(0)" class="btn btn-primary" id="pumpreport"><i class="mdi-action-settings-power"></i>Pump readings</a>
-    <a href="javascript:void(0)" class="btn btn-primary" id="lubricantreport"><i class="mdi-action-settings-power"></i>Lubricant sales</a>
+    <a href="javascript:void(0)" class="btn btn-primary" id="pumpreport"><i class="mdi-action-assignment"></i>Pump readings</a>
+    <a href="javascript:void(0)" class="btn btn-primary" id="lubricantreport"><i class="mdi-action-note-add"></i>Lubricant sales</a>
 </div>
 
     <script>
