c87d1357c23cd1c6a64668182786474615709c0a
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-09-25 22:40:13 +0530

    lube pdfs

--- views/lubricantService/pdf/RegHistory.php
+++ views/lubricantService/pdf/RegHistory.php
@@ -6,8 +6,20 @@ $pdf = new FPDF();
 
 $pdf->AddPage();
 
+$pdf->SetFont('Arial', 'B', 18);
+//formatting pdf
+
+$pdf->Image('views/img/ioc.png', 95, 1, 20);
+$pdf->MultiCell(20, 20, "");
+$pdf->Text(45, 28, 'Regular Customer Transactions Report');
+
+//table data
 $pdf->SetFont('Arial', 'B', 8);
 
+date_default_timezone_set('Asia/Colombo'); //to get srilankan time instead of GMT time
+$date = date("l jS \of F Y h:i:s A");
+
+$pdf->Text(5, 5, ' Date: ' . $date);
 
 
 $heading = array("Customer", "VehicleNO", "Amount Rs.", "OrderDate");
