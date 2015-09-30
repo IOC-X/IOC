378c12ddcb2294f49d807af516ea661b48a97953
Rajika <inbox.rajika@gmail.com>
2015-09-30 09:16:56 +0530

    Err

--- libs/Database.php
+++ libs/Database.php
@@ -2,11 +2,7 @@
 	class Database extends PDO{
 		function __construct(){
 			try{
-<<<<<<< HEAD
 				parent::__construct('mysql:host=localhost;dbname=IOC2','root','miyoungrae123');
-=======
-				parent::__construct('mysql:host=localhost;dbname=IOC','root','');
->>>>>>> 692d359a2bb7a0bd3cad5f669e63c907c3479308
 			}
 			catch(PDOException $e){
 				echo $e;
